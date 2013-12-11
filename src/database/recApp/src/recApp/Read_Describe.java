c73b16b5631dc0626fdc370e39cb9bfe8881ba0c
bargl1991 <bartosz.bargiel@uj.edu.pl>
2013-12-11 22:02:43 +0100

    Zmodyfikowanie aplikacji do dodawania kategorii i poprawienie oraz dodanie przepisow

--- src/database/recApp/src/recApp/Read_Describe.java
+++ src/database/recApp/src/recApp/Read_Describe.java
@@ -1,4 +1,5 @@
 package recApp;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,13 +18,69 @@ public class Read_Describe {
 	private static Document doc;
 	private static PrintWriter zapis;
 	private static BufferedImage image;
+	private static Scanner in = new Scanner(System.in);
 
 	public static void readDane(String file) throws IOException {
 		try {
 			zapis = new PrintWriter(file + ".txt");
 			doc = Jsoup.connect(urlText).get();
 			String tmp = "";
+			boolean tr = true;
 
+			System.out.println("KATEGORIE");
+			System.out.println("1) mieso i wedliny");
+			System.out.println("2) ryby i owoce morza");
+			System.out.println("3) nabial");
+			System.out.println("4) warzywa");
+			System.out.println("5) owoce");
+			System.out.println("6) pieczywo");
+			System.out.println("7) grzyby");
+			System.out.println("8) zupy");
+			System.out.println("9) sosy");
+			System.out.println("10) przetwory");
+			System.out.println("11) desery");
+			System.out.println("12) napoje");
+			System.out.println("13) salatki");
+			System.out.println("14) studenckie");
+			System.out.println("0) konczy");
+			while(tr){
+			pisz("Podaj kategorie: ");
+			switch(in.nextInt()){
+			case 0: tr = false;
+			break;
+			case 1: zapis.println("<cat>mieso<cat>");
+			break;
+			case 2: zapis.println("<cat>ryby<cat>");
+			break;
+			case 3: zapis.println("<cat>nabial<cat>");
+			break;
+			case 4: zapis.println("<cat>warzywa<cat>");
+			break;
+			case 5: zapis.println("<cat>owoce<cat>");
+			break;
+			case 6: zapis.println("<cat>pieczywo<cat>");
+			break;
+			case 7: zapis.println("<cat>grzyby<cat>");
+			break;
+			case 8: zapis.println("<cat>zupy<cat>");
+			break;
+			case 9: zapis.println("<cat>sosy<cat>");
+			break;
+			case 10: zapis.println("<cat>przetwory<cat>");
+			break;
+			case 11: zapis.println("<cat>desery<cat>");
+			break;
+			case 12: zapis.println("<cat>napoje<cat>");
+			break;
+			case 13: zapis.println("<cat>salatki<cat>");
+			break;
+			case 14: zapis.println("<cat>studenckie<cat>");
+			break;
+			default: pisz("Podany numer jest bledny. Sprobuj jeszcze raz.");
+			break;
+			}
+			}
+			
 			/**
 			 * Bierze nazwe przepisu
 			 */
@@ -62,7 +119,7 @@ public class Read_Describe {
 			tmp = "http://www.kulinarne-odkrycia.pl/" + tmp;
 			String tmp2[] = tmp.split(" ");
 			tmp2[0]=tmp2[0].substring(0, tmp2[0].length()-1);
-			pisz(tmp2[0]);
+			//pisz(tmp2[0]);
 			URL net = new URL(tmp2[0]); 
 			image = ImageIO.read(net);
 			File imageFile = new File(file + ".jpg");
@@ -80,10 +137,8 @@ public class Read_Describe {
 		System.out.println(txt);
 	}
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 
-		Scanner in = new Scanner(System.in);
 		int s, n;
 
 		pisz("Starting create amount: ");
