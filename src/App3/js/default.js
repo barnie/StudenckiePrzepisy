b56db71f4e69524fe89293e4a24a38d02c2d66f3
barnie <piotr@kruk.co>
2013-12-17 22:46:04 +0100

    Porzadki

--- src/App3/js/default.js
+++ src/App3/js/default.js
@@ -28,23 +28,13 @@
 
     function buttonClickHandler(eventInfo) {
         // that button test a database only :) Cool and chill nigga!
-         createDB();
-      /*  var tab = [];
+         //createDB();
+        var tab = [];
         tab[0] = new Array(); tab[1] = new Array();
         tab[0][0] = 1; tab[0][1] = 'szklanka'; tab[0][2] = 2;
         tab[1][0] = 2; tab[1][1] = 'lyzka'; tab[1][2] = 3;
-         addPrzepis(1,'aaa','xxx','img',tab) */
-       /* var array = [];
-        getKategorie(array).then(function () {
-            for (var i = 0; i < array.length; i++)
-                console.log(array[i]);
-        })*/
-        var array = [];
-        /*getSkladnik(array).then(function () {
-            for (var i = 0; i < array.length; ++i)
-                console.log(array[i]);
-        });*/
-        addSkladnik('xD');
+        addPrzepis(1,'ala ma psa','xxx','img',tab) 
+      
         
     }
 
