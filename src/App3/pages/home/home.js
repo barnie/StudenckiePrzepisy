9a058abfde8eaab7289bc2c1d54b3ab57b11baea
Marcin Milik <mailo.poland@gmail.com>
2013-12-21 17:16:09 +0100

    poprawione home v2.0 :)

--- src/App3/pages/home/home.js
+++ src/App3/pages/home/home.js
@@ -3,9 +3,6 @@
 
     WinJS.UI.Pages.define("/pages/home/home.html", {
         ready: function (element, options) {
-             document.querySelector("#cat").onclick = function (args) { //button
-                WinJS.Navigation.navigate("/pages/recipe/recipe.html", "smigam z home jooo");
-            };
 
            WinJS.Utilities.query("a").listen("click", anchorHandler, false);
         }
@@ -28,8 +25,8 @@
         }
         else {
             var array = [];
-            getOnePrzepis("ala", array).then(function(){
-                WinJS.Navigation.navigate(link.href);
+            getOnePrzepis("ala", array).then(function () {
+                WinJS.Navigation.navigate(link.href , array);
             })
         }
     }
