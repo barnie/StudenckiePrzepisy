8d6882eff4c29c17a95fcf68a55ad9083a2c71b8
Marcin Milik <mailo.poland@gmail.com>
2013-12-21 16:45:59 +0100

    recipe.html wyswietla dane z db przekazane przez funkcje getOnePrzepis()

--- src/App3/pages/recipe/recipe.js
+++ src/App3/pages/recipe/recipe.js
@@ -9,8 +9,18 @@
         ready: function (element, options) {
             // TODO: Initialize the page here.
             //options to przekazany parametr ze strony wywolujacej (parenta)
-            console.log(options);
-
+            document.getElementById("category").innerHTML = options[1];
+            console.log("option[1]=" + options);
+            document.getElementById("name").innerHTML = options[2];
+            document.getElementById("img_container").innerHTML = window.toStaticHTML("<img src=" + options[4] + " />");
+            console.log("options[4]=" + options[4]);
+            document.getElementById("description").innerHTML = options[3];
+            //skladniki:
+            var container = "<ul>";
+            for( var i = 5; i < options.length ; i++)
+                container += "<li>" + options[i][0] + " - " + options[ i ][1] + options[i][2] + "</li>";
+            container += " </ul>";
+            document.getElementById("list").innerHTML = window.toStaticHTML(container);
         },
 
         unload: function () {
