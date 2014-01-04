afb0480897cdfae7bcbbbcf668634e3a28fd7493
Marcin Milik <mailo.poland@gmail.com>
2014-01-04 19:19:51 +0100

    poprawienie wyswietlania kategorii (mile widziane uwagi jak to na roznych rozdzialkach wyglada)

--- src/App3/pages/categories/categories.js
+++ src/App3/pages/categories/categories.js
@@ -9,39 +9,47 @@
             
             //script:
 
-                //date prepare:
-                var categories = options;
-                var count = options.length;
+            //date prepare:
+            var categories = options;
+            var count = options.length;
             //picturesPaths bedzie pobierane z bazy (kazda kategoria bedzie miala swoj obrazek, niezalezny od przepisow), narazie przykladowe wart:
-                var picturesPaths = [ "miecho.jpg", "ryba.jpg", "nabial.jpg", "warzywa.jpg", "owoce.jpg", "pieczywo.jpg", "grzyby.jpg", "soup.jpg", "sos.jpg", "przetwory.jpg", "deser.jpg", "napoj.jpg", "salatka.jpg", "studenckie.jpg"];
-                //date prepared, start script:
-                var layout = new LayoutContainer(count, 'myTabCss', 'myRowCss', 'myCellCss');
-                var table = layout.CreateContainer();
+            var picturesPaths = [ "miecho.jpg", "ryba.jpg", "nabial.jpg", "warzywa.jpg", "owoce.jpg", "pieczywo.jpg", "grzyby.jpg", "soup.jpg", "sos.jpg", "przetwory.jpg", "deser.jpg", "napoj.jpg", "salatka.jpg", "studenckie.jpg"];
+            //date prepared, start script:
+            var layout = new LayoutContainer(count, 'myTabCss', 'myRowCss', 'myCellCss');
+            var table = layout.CreateContainer();
 
-                if (count < 3)
-                    document.getElementById('myDiv').innerHTML = window.toStaticHTML('<br><br>' + table.toString());
-                else
-                    document.getElementById('myDiv').innerHTML = window.toStaticHTML(table.toString());
-                var prefix = 'first';
-                var j = 0;
-                var imgTagsIds = new Array();
-                var nameTagsIds = new Array();
-                for (var i = 0; i < count; i++) {
-                    var image = layout.CreateElementContent(prefix + j.toString(), picturesPaths[i], 'myImageCss', categories[i][0], categories[i][1]);
-                    imgTagsIds[i] = prefix + j.toString() + 'img';
-                    nameTagsIds[i] = prefix + j.toString() + 'name';
-                    if (j == layout.width) {
-                        j = 0;
-                        prefix = "second";
-                    }
+            if (count < 3)
+                document.getElementById('myDiv').innerHTML = window.toStaticHTML('<br><br>' + table.toString());
+            else
+                document.getElementById('myDiv').innerHTML = window.toStaticHTML(table.toString());
+            var prefix = 'first';
+            var j = 0;
+            var imgTagsIds = new Array();
+            var nameTagsIds = new Array();
+            for (var i = 0; i < count; i++) {
+                var image = layout.CreateElementContent(prefix + j.toString(), picturesPaths[i], 'myImageCss', categories[i][0], categories[i][1]);
+                imgTagsIds[i] = prefix + j.toString() + 'img';
+                nameTagsIds[i] = prefix + j.toString() + 'name';
+                if (j == layout.width) {
+                    j = 0;
+                    prefix = "second";
+                }
 
-                    document.getElementById(prefix + j.toString()).innerHTML = window.toStaticHTML(image.toString());
-                    j++;
-                }
-                for (var i = 0; i < count; i++) {
-                    element.querySelector('#' + imgTagsIds[i]).onclick = this.loadRecipe;
-                    element.querySelector('#' + nameTagsIds[i]).onclick = this.loadRecipe;
-                }
+                document.getElementById(prefix + j.toString()).innerHTML = window.toStaticHTML(image.toString());
+                j++;
+            }
+            for (var i = 0; i < count; i++) {
+                element.querySelector('#' + imgTagsIds[i]).onclick = this.loadRecipe;
+                element.querySelector('#' + nameTagsIds[i]).onclick = this.loadRecipe;
+            }
+            //additional data for css prepare:
+            document.getElementById("container").style.borderSpacing = "" + window.screen.availWidth * 0.028 + "px 0 ";
+            for (var i = 0; i < 7; i++) {
+                document.getElementById('first' + i.toString() ).style.width = " 1 px";
+                document.getElementById('second' + i.toString()).style.width = " 1 px";
+            }
+
+
         },
         loadRecipe: function (arg) {
             arg = $(this).data('arg');
