2edd1c218ebf05c0183393bda2411f6c8eac3014
barnie <piotr@kruk.co>
2014-01-30 20:10:30 +0100

    Czystki

--- src/App3/bin/x86/Debug/AppX/js/ParseHelper.js
+++
@@ -1,7 +0,0 @@
-﻿function ParseNameToFullUrl(name)
-{
-    var p = Windows.Storage.ApplicationData.current.localFolder.path;
-    if (p.charAt(p.length - 1) != '\\') p += '\\';
-    return p + name;
-}
-
