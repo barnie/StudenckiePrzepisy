b1b39ae4684109e7258d1717ebf3077e1c25b684
skipper <piotr@kruk.co>
2014-06-01 23:00:18 +0200

    #Android: Add: Sending recipe via sms/mms or email

--- src/StudenckiePrzepisy/src/com/example/studenckieprzepisy/Przeepis.java
+++ src/StudenckiePrzepisy/src/com/example/studenckieprzepisy/Przeepis.java
@@ -5,10 +5,15 @@ import Database.Przepis;
 import Database.PrzepisSkladnik;
 import Database.Skladnik;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +30,8 @@ public class Przeepis extends Activity {
     private ImageView zdjecie;
     private TextView skladniki;
     private TextView opis;
+    private Przepis p;
+    private String output = "";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +43,7 @@ public class Przeepis extends Activity {
         String przepis = getIntent().getExtras().getString("KEY");
         getActionBar().setTitle(przepis);
         Database db = new Database(getApplicationContext(), null, null, 1);
-        Przepis p = db.getPrzepis(przepis);
+        p = db.getPrzepis(przepis);
         opis.setText(p.getOpis());
         AssetManager assetManager = getAssets();
         try {
@@ -57,7 +64,6 @@ public class Przeepis extends Activity {
         for (Skladnik s : skladnik) {
             Log.d("###", s.toString());
         }
-        String output = "";
         for (PrzepisSkladnik tmp : ps) {
             if (tmp.getIle().compareTo("") != 0)
                 output += skladnik.get(tmp.getIdskladnik() - 1).getNazwa() + " : " + tmp.getIle() + "  " + tmp.getMiara() + "\n";
@@ -65,5 +71,49 @@ public class Przeepis extends Activity {
                 output += skladnik.get(tmp.getIdskladnik() - 1).getNazwa() + " : ile uznasz za stosowne \n";
         }
         skladniki.setText(output);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_przepis, menu);
+        //menu.add(0, R.id.add_przepis, 0, "Wyslij przepis smsem");
+        return true;
+    }
+
+    public void sendSms() {
+        Intent smsIntent = new Intent(android.content.Intent.ACTION_SEND);
+        smsIntent.putExtra("sms_body", p.getNazwa() + "\n" + output + "\n" + p.getOpis());
+        if (p.getZdjecie() != null && p.getZdjecie().compareTo("") != 0 && p.getZdjecie().length() > 9) {
+            smsIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + p.getZdjecie()));
+            smsIntent.setType("image/png");
+        }
+        startActivity(Intent.createChooser(smsIntent, "Wybierz swojego klienta sms/mms"));
+    }
+
+    public void sendEmail() {
+        final Intent result = new Intent(Intent.ACTION_SEND);
+        result.putExtra(android.content.Intent.EXTRA_SUBJECT, "Przepis :" + p.getNazwa());
+        result.putExtra(android.content.Intent.EXTRA_TEXT, p.getNazwa() + "\n" + output + "\n" + p.getOpis());
+        if (p.getZdjecie() != null && p.getZdjecie().compareTo("") != 0 && p.getZdjecie().length() > 9) {
+            result.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + p.getZdjecie()));
+            result.setType("image/png");
+        }
+        result.setType("message/rfc822");
+        startActivity(Intent.createChooser(result, "Wybierz swojego klienta email :"));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.wyslij_sms:
+                sendSms();
+                break;
+            case R.id.wyslij_email:
+                sendEmail();
+                break;
+        }
+        return true;
     }
 }
